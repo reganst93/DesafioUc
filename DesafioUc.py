67a993bfe4fc83cce153ede7842f1a211cce0f8a
reganst93 <stmi.93@gmail.com>
2023-10-02 21:35:53 -0300

    primer commit

--- DesafioUc.py
+++ DesafioUc.py
@@ -10,8 +10,8 @@ with open('MOCK_DATA.json') as file:
     
 #Conexion sqlite 
 
-conexion = sqlite3.connect('proyectouc.sqlite')
+conexion = sqlite3.connect('proyectoUc.sqlite')
 cursorBD = conexion.cursor()
 
 #Verificamos si la tabla de base de datos existe
-cursorBD.execute(''' CREATE TABLE PROJECT(ID INTEGER PRIMARY KEY AUTOINCREMENT,FIRST_NAME TEXT,LAST NAME TEXT,EMAIL TEXT,GENDER TEXT,PLAN DE SALUD TEXT,PHONE INTEGER )''')
+cursorBD.execute(''' CREATE TABLE PROYECTOUC(ID INTEGER PRIMARY KEY AUTOINCREMENT,FIRST_NAME TEXT,LAST NAME TEXT,EMAIL TEXT,GENDER TEXT,PLAN DE SALUD TEXT,PHONE INTEGER )''')

9cd20e19383ffb5f0943caef4c04b2d38b8679f4
reganst93 <stmi.93@gmail.com>
2023-10-03 12:47:47 -0300

    exportacion de Mock_DATA

--- DesafioUc.py
+++ DesafioUc.py
@@ -1,17 +1,43 @@
-import sqlite3 
+import sqlite3
 import json
 
+#conexion sqlite 
+
+conexion = sqlite3.connect('proyectouc.sqlite')
+cursor = conexion.cursor()
+
+#Verificamos si la tabla de base de datos existe
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Project_UC (
+    FirstName TEXT,
+    LastName TEXT,
+    Email TEXT,
+    Gender TEXT,
+    PlanDeSalud TEXT,
+    Phone INTEGER
+)
+''')
 
 #Carga de datos desde el archivo JSON
 
-with open('MOCK_DATA.json') as file:
-    data = json.load(file)
-    
-    
-#Conexion sqlite 
+with open('MOCK_DATA.json', 'r') as archivo:
+    datos_json = json.load(archivo)
 
-conexion = sqlite3.connect('proyectoUc.sqlite')
-cursorBD = conexion.cursor()
+#Insertar datos de registro en tablas
 
-#Verificamos si la tabla de base de datos existe
-cursorBD.execute(''' CREATE TABLE PROYECTOUC(ID INTEGER PRIMARY KEY AUTOINCREMENT,FIRST_NAME TEXT,LAST NAME TEXT,EMAIL TEXT,GENDER TEXT,PLAN DE SALUD TEXT,PHONE INTEGER )''')
+for id, dato in enumerate(datos_json):
+    if id < 500:
+        cursor.execute(''' 
+            INSERT INTO Project_UC (FirstName, LastName, Email, Gender, PlanDeSalud, Phone)
+            VALUES (?,?,?,?,?,?)
+        ''', (dato['first_name'], dato['last_name'], dato['email'], dato['gender'], dato['Plan de Salud'], dato['phone']))
+    else:
+        break
+ 
+   
+
+ # realiza un commit para guardar los cambios en la base de datos 
+conexion.commit()
+
+#cierra la conexion a la base de datos cuando hayas termiado 
+conexion.close()
